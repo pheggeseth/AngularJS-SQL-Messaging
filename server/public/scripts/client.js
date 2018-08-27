d1dcccdbc4e257ccbb629692d63c27ee5a1f09c6
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-27 14:53:24 -0500

    Messages display on DOM. Form adds message to DB and DOM.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,30 @@ const app = angular.module('app', []);
 app.controller('MessagesController', ['$http', function($http) {
   vm = this;
   vm.hello = 'Hello from MessagesController';
+  vm.messageForm = {
+    name: '',
+    message: ''
+  };
+  vm.messages = [];
+
+  vm.getMessages = function() {
+    $http.get('/messages').then(function(response) {
+      console.log('/messages GET success:', response.data);
+      vm.messages = response.data;
+    }).catch(function(error) {
+      console.log('/messages GET error:', error);
+    });
+  };
+
+  vm.addMessage = function() {
+    console.log(vm.messageForm);
+    $http.post('/messages', vm.messageForm).then(function(response) {
+      console.log('/messages POST success:', response);
+      vm.getMessages();
+    }).catch(function(error) {
+      console.log('/messages POST error:', error);
+    });
+  };
+
+  vm.getMessages();
 }]);
--- server/routes/messages.router.js
+++ server/routes/messages.router.js
@@ -16,8 +16,9 @@ const pool = new Pool(config);
 pool.on('connect', () => console.log('postgresql connected!!!'));
 pool.on('error', error => console.log('Error connecting to db', error));
 
+// '/messages' ROUTES
 
-// get route params with "/route/:paramName, then reference it as req.params.paramName"
+// GET ALL MESSAGES
 router.get('/', (req, res) => {
   console.log('/messages GET hit');
   const queryText = `SELECT * FROM "messages";`;
@@ -29,6 +30,7 @@ router.get('/', (req, res) => {
     });
 });
 
+// ADD NEW MESSAGE
 router.post('/', (req, res) => {
   const messageToAdd = req.body; // This the data we sent
     console.log('/messages POST hit:', messageToAdd); // Has a name, size and cost
@@ -39,46 +41,6 @@ router.post('/', (req, res) => {
         console.log('Error in POST:', error);
         res.sendStatus(500);
     });
-});
-
-router.put('/:id', (req, res) => {
-  /* 
-  Model.findOne({_id: req.params.id})
-    .then(foundModel => {
-      //alter model then save in database
-    }).catch(error => res.sendStatus(500));
-  */
-
-  // POSTGRESQL SAMPLE PUT
-  //  const updatedShoe = req.body;
-  //  const queryText = `UPDATE "shoes" 
-  //                     SET "name" = $1, "cost" = $2, "size" = $3
-  //                     WHERE "id" = $4;`;
-  //  pool.query(queryText, [updatedShoe.name,
-  //                         updatedShoe.cost, 
-  //                         updatedShoe.size, 
-  //                         updatedShoe.id]).then( (result) => {
-  //                             res.sendStatus(200);
-  //                         }).catch( (error) => {
-  //                             res.sendStatus(500);
-  //                         });
-});
-
-router.delete('/:id', (req, res) => {
-  // Model.findByIdAndRemove(req.params.id)
-  //  .then(response => res.sendStatus(201))
-  //  .catch(() => res.sendStatus(500));
-
-  // POSTGRESQL SAMPLE DELETE
-  // const idOfShoeToDelete = req.params.id;
-  //   console.log('deleting ', idOfShoeToDelete);
-  //   const queryText = 'DELETE FROM "shoes" WHERE "id" = $1;';
-  //   pool.query(queryText, [idOfShoeToDelete]).then((result) => {
-  //       res.sendStatus(200);
-  //   }).catch( (error) => {
-  //       console.log('Error in delete', error);
-  //       res.sendStatus(500);
-  //   });
 });
 
 module.exports = router;
