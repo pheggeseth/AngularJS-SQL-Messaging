a14a0c481af40df1590dd1ae027afb433bcbca4d
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-27 14:33:19 -0500

    Initial commit. Angular and controller initialized.

---
+++ server/public/scripts/client.js
@@ -0,0 +1,20 @@
+/*
+When first installing app, run from command line:
+> npm init --yes
+> npm install express --save
+
+Install DB interface package
+> npm install mongoose --save (for MongoDB)
+> npm install pg --save (for PostgreSQL)
+
+in separate terminal window, start database
+> mongod for MongoDB
+> brew services start postgresql for PostgreSQL
+*/
+console.log('JS');
+
+const app = angular.module('app', []);
+app.controller('MessagesController', ['$http', function($http) {
+  vm = this;
+  vm.hello = 'Hello from MessagesController';
+}]);
